d638647364fc12ce846718110abb6a2c4166ee2c
Tejal Kadam <tejalmchandlekar@gmail.com>
2025-07-11 12:42:54 +0530

    added code for Base Url

--- src/components/VerifyEmail.js
+++ src/components/VerifyEmail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import axios from 'axios';   
 import BASE_URL from '../baseUrl';
 
 function VerifyEmail() {
